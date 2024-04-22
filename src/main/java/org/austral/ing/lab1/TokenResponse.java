ba68504c7e1adf128a9fab0eaaa7c1f2930d30d8
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-04-22 12:10:52 -0300

    Token response es el back generando el token y asociándolo con el email

--- src/main/java/org/austral/ing/lab1/TokenResponse.java
+++ src/main/java/org/austral/ing/lab1/TokenResponse.java
@@ -1,18 +1,58 @@
 package org.austral.ing.lab1;
+
+import java.util.Base64;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
 public class TokenResponse {
-    private String token;
-    private String refreshToken;
+    private static final Key JWT_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final Set<String> expiredTokens = new HashSet<>();
 
-    public TokenResponse(String token, String refreshToken) {
-        this.token = token;
-        this.refreshToken = refreshToken;
+    public static String generateToken(String email) {
+        return Jwts.builder().setSubject(email).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(JWT_KEY).compact();
     }
 
-    public String getToken() {
-        return token;
+    public static String getUserEmail(String token){
+        try { System.out.println("Secret key: " + Base64.getEncoder().encodeToString(JWT_KEY.getEncoded()));
+            System.out.println("Token: " + token);
+            return Jwts.parserBuilder().setSigningKey(JWT_KEY).build().parseClaimsJws(token).getBody().get("email", String.class);
+        } catch (Exception e) {
+            System.out.println("Error al decodificar el token: " + e.getMessage());
+            return null;
+        }
     }
 
-    public String getRefreshToken() {
-        return refreshToken;
+    public static void invalidateToken(String token){
+        expiredTokens.add(token);
+    }
+
+    public static boolean isTokenInvalid(String token){
+        return expiredTokens.contains(token);
+    }
+
+    public static boolean isAuthorized(String token, String requestedEmail){
+
+        // Verificar si el token es válido
+        if (isTokenInvalid(token)) {
+            return false;
+        }
+        // Obtener el correo electrónico asociado al token
+        String userEmail = getUserEmail(token);
+
+        // Verificar si el correo electrónico obtenido está vacío o nulo
+        if (userEmail == null || userEmail.isEmpty()) {
+            return false;
+        }
+        // Verificar si el correo electrónico del token coincide con el correo solicitado
+        return userEmail.equals(requestedEmail);
     }
 }

844c59fdd5dd4ba0e5abeb3aecf99e767e9dbc9d
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-07-06 15:03:24 -0300

    button

--- src/main/js/front/app/tabs/House/House.jsx
+++ src/main/js/front/app/tabs/House/House.jsx
@@ -96,8 +96,9 @@ export default function House({navigation}) {
                     </View>
                     <p></p>
                     <View style={styles.linksContainer}>
+                        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                             <Pressable onPress={() => navigation.navigate("LowOnStock")}>
-                                <Text style={styles.link}>Low stock</Text>
+                                <Text style={styles.link}>Low on stock products</Text>
                             </Pressable>
                             <Pressable onPress={() => navigation.navigate("AddStock")}>
                                 <Text style={styles.link}>Add a Product</Text>
@@ -105,6 +106,8 @@ export default function House({navigation}) {
                             <Pressable onPress={()=> navigation.navigate("RegisterProduct")}>
                                 <Text style={styles.link}>Create a Product</Text>
                             </Pressable>
+                        </View>
+                        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                             <Pressable onPress={()=> navigation.navigate("Homes")}>
                                 <Text style={styles.link}>Select another home</Text>
                             </Pressable>
@@ -114,6 +117,7 @@ export default function House({navigation}) {
                             <Pressable onPress={()=> navigation.navigate("HouseUsersPage")}>
                                 <Text style={styles.link}>Manage users</Text>
                             </Pressable>
+                        </View>
                     </View>
                     <Tuple navigation={navigation}/>
 

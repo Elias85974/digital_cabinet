f3dc91faf1d81db5ef0f931e0100e1f1b0f57524
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-07-10 01:10:37 -0300

    add button

--- src/main/js/front/app/tabs/House/Product/Products.jsx
+++ src/main/js/front/app/tabs/House/Product/Products.jsx
@@ -163,6 +163,9 @@ export default function Product({navigation}) {
                                     <Pressable onPress={() => setModalVisible3(true)}>
                                         <Text style={styles.link}>Reduce Stock</Text>
                                     </Pressable>
+                                    <Pressable onPress={() => navigation.navigate('AddStock')}>
+                                        <Text style={styles.link}>Add Stock</Text>
+                                    </Pressable>
                                     <Pressable onPress={() => setModalVisible2(false)} >
                                         <Text style={styles.link}>Cerrar</Text>
                                     </Pressable>

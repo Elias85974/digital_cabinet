f780a5a2cafd134cb86e2e1c08406dbba0bfa619
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-07-10 02:40:28 -0300

    pichart y varios

--- src/main/js/front/app/api/piechart.js
+++ src/main/js/front/app/api/piechart.js
@@ -4,27 +4,3 @@ import {FetchApi} from "../Api";
 export const getInventoryValueByCategory = async (houseId, navigation) => {
     return await FetchApi.getFetch(`/houses/${houseId}/inventory/valueByCategory`, 'Failed to get inventory value by category:', navigation);
 }
-
-
-import { API_URL } from '../constants';
-
-
-// Function that gets the list of products of a house and a category
-export const getProductsFromHouseAndCategory = async (houseId, category) => {
-    try {
-        const response = await fetch(`${API_URL}/houses/${houseId}/products/${category}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        if (!response.ok) {
-            const errorMessage = await response.text();
-            throw new Error(errorMessage);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error("Failed to get products:", error);
-        throw error;
-    }
-};

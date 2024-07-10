15dd7cb7893d427407077a1d00db4117be78eeee
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-07-10 00:37:38 -0300

    pie chart (arreglos juli)

--- src/main/js/front/app/tabs/House/Product/PieChart.jsx
+++ src/main/js/front/app/tabs/House/Product/PieChart.jsx
@@ -3,39 +3,41 @@ import {Pie} from 'react-chartjs-2';
 import {ArcElement, Chart, Legend, Tooltip} from 'chart.js';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {getInventoryValueByCategory, getProductsFromHouseAndCategory} from '../../../api/piechart';
-import {Picker, SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native";
+import {FlatList, Picker, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import NavBar from "../../NavBar/NavBar";
+import {useIsFocused} from "@react-navigation/native";
 
 Chart.register(ArcElement, Tooltip, Legend);
 
 export default function PieChartComponent({navigation}){
+    const chartColors = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(199, 199, 199, 0.2)',
+        'rgba(83, 102, 255, 0.2)',
+    ];
+    const borderColors = [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(199, 199, 199, 1)',
+        'rgba(83, 102, 255, 1)',
+    ];
+
     const [chartData, setChartData] = useState({
         labels: [],
         datasets: [{
-            label: 'Total Value ',
+            label: 'Total Values',
             data: [],
-            backgroundColor: [
-                // Define your colors for each category
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(199, 199, 199, 0.2)',
-                'rgba(83, 102, 255, 0.2)',
-            ],
-            borderColor: [
-                // Define border colors for each category
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(199, 199, 199, 1)',
-                'rgba(83, 102, 255, 1)',
-            ],
+            backgroundColor: chartColors,
+            borderColor: borderColors,
             borderWidth: 1,
         }],
 
@@ -45,151 +47,102 @@ export default function PieChartComponent({navigation}){
     const [selectedCategory, setSelectedCategory] = useState('');
     const [total, setTotal] = useState(0);
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-            try {
-                const houseId = await AsyncStorage.getItem('houseId');
-                if (houseId) {
-                    const inventoryValueResponse = await getInventoryValueByCategory(houseId);
-                    const fetchedCategories = Object.keys(inventoryValueResponse);
-                    console.log('Fetched categories:', fetchedCategories);
-                    setCategories(fetchedCategories);
-                    if (fetchedCategories.length > 0) {
-                        setSelectedCategory(fetchedCategories[0]);
-                    }
-                }
-            } catch (error) {
-                console.error('Error fetching categories:', error);
-            }
-        };
-
-        fetchCategories();
-    }, []);
+    const isFocused = useIsFocused();
 
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const houseId = await AsyncStorage.getItem('houseId');
-                if (houseId && selectedCategory) {
-                    const response = await getProductsFromHouseAndCategory(houseId, selectedCategory);
-                    console.log("Response received:", response);
-                    if (response && Array.isArray(response)) {
-                        const dataArray = response.map((item) => {
-                            console.log('Item:', item);
-                            const categoryName = selectedCategory; // Use the selectedCategory state
-                            const productName = item.product.nombre;
-                            return {
-                                product: productName,
-                                value: item.price,
-                            };
-                        });
-                        console.log('Data array:', dataArray);
-                        // Calculate total value
-                        const totalValue = dataArray.reduce((acc, item) => acc + item.value, 0);
-                        // Calculate percentages and update labels with percentages
-                        const categoriesWithPercentages = dataArray.map((item) => {
-                            //const percentage = ((item.value / totalValue) * 100).toFixed(2);
-                            return `${item.product}`;
-                        });
-                        const values = dataArray.map((item) => item.value);
-                        const newChartData = {
-                            labels: categoriesWithPercentages,
-                            datasets: [{
-                                label: 'Total Value',
-                                data: values,
-                                backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 206, 86, 0.2)',
-                                    'rgba(75, 192, 192, 0.2)',
-                                    'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)',
-                                    'rgba(199, 199, 199, 0.2)',
-                                    'rgba(83, 102, 255, 0.2)',
-                                ],
-                                borderColor: [
-                                    'rgba(255, 99, 132, 1)',
-                                    'rgba(54, 162, 235, 1)',
-                                    'rgba(255, 206, 86, 1)',
-                                    'rgba(75, 192, 192, 1)',
-                                    'rgba(153, 102, 255, 1)',
-                                    'rgba(255, 159, 64, 1)',
-                                    'rgba(199, 199, 199, 1)',
-                                    'rgba(83, 102, 255, 1)',
-                                ],
-                                borderWidth: 1,
-                            }],
-                        };
-                        setChartData(newChartData);
-                        // After calculating newChartData, calculate total expense
-                        const totalExpense = values.reduce((acc, currentValue) => acc + currentValue, 0);
-                        setTotal(totalExpense); // Update total state
-
-                    } else {
-                        console.error('Expected an array but received:', response);
-                    }
-                }
-            } catch (error) {
-                console.error('Error fetching inventory value by category:', error);
-            }
-        };
-
-        fetchData();
-    }, [selectedCategory]);
+        if (isFocused) {
+            getTotalData();
+            fetchCategories();
+        }
+    }, [isFocused]);
 
     const handleInputChange = (name, value) => {
         console.log('Selected category:', value);
         setSelectedCategory(value);
-        if (value === 'totalValues') {
-            fetchTotalData(); // This function should fetch and aggregate data across all categories
+        if (value === 'Total Values') {
+            getTotalData(); // This function should fetch and aggregate data across all categories
         } else {
-            fetchData(); // This function should fetch data for the selected category
+            getDataFromCategory(value); // This function should fetch data for the selected category
         }
     };
 
-    const fetchTotalData = async () => {
-        try {
-            const houseId = await AsyncStorage.getItem('houseId');
-            if (houseId) {
-                const response = await getInventoryValueByCategory(houseId);
-                if (response && typeof response === 'object' && !Array.isArray(response)) {
-                    const totalData = Object.entries(response).map(([category, value]) => ({
-                        category,
-                        value
+    const getDataFromCategory = async (selectedCategory) => {
+        const houseId = await AsyncStorage.getItem('houseId');
+        if (houseId && selectedCategory) {
+            try {
+                const response = await getProductsFromHouseAndCategory(houseId, selectedCategory);
+                if (response && Array.isArray(response)) {
+                    const dataArray = response.map(item => ({
+                        product: item.product.nombre,
+                        value: item.price,
                     }));
-                    const totalValue = totalData.reduce((acc, item) => acc + item.value, 0);
-                    const categoriesWithPercentages = totalData.map(item => {
-                        //const percentage = ((item.value / totalValue) * 100).toFixed(2);
-                        return `${item.category} `;
-                    });
-                    const values = totalData.map(item => item.value);
-                    const totalChartData = {
-                        labels: categoriesWithPercentages,
+                    const labels = dataArray.map(item => item.product);
+                    const values = dataArray.map(item => item.value);
+                    const chartData = {
+                        labels,
                         datasets: [{
                             label: 'Total Value',
                             data: values,
-                            backgroundColor: chartData.datasets[0].backgroundColor,
-                            borderColor: chartData.datasets[0].borderColor,
+                            backgroundColor: chartColors,
+                            borderColor: borderColors,
                             borderWidth: 1,
                         }],
                     };
-                    setChartData(totalChartData);
-                    // After calculating totalChartData, calculate total expense
-                    const totalExpense = values.reduce((acc, currentValue) => acc + currentValue, 0);
-                    setTotal(totalExpense); // Update total state
-
+                    setChartData(chartData);
+                    setTotal(values.reduce((acc, currentValue) => acc + currentValue, 0));
                 } else {
-                    console.error('Invalid response:', response);
+                    console.error('Expected an array but received:', response);
                 }
-            } else {
-                console.error('No house ID found');
+            } catch (error) {
+                console.error('Error fetching data:', error);
             }
-        } catch (error) {
-            console.error('Error fetching inventory value by category:', error);
         }
     };
 
+    const fetchCategories = async () => {
+        try {
+            const houseId = await AsyncStorage.getItem('houseId');
+            if (houseId) {
+                const inventoryValueResponse = await getInventoryValueByCategory(houseId);
+                let fetchedCategories = Object.keys(inventoryValueResponse);
+                fetchedCategories.unshift('Total Values'); // Agrega 'Total Values' al inicio del array
+                setCategories(fetchedCategories);
+                setSelectedCategory(fetchedCategories[0]); // Esto seleccionará 'Total Values' por defecto
+            }
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+    };
+
+    const getTotalData = async () => {
+        const houseId = await AsyncStorage.getItem('houseId');
+        if (!houseId) return;
+        try {
+            const response = await getInventoryValueByCategory(houseId);
+            if (response && typeof response === 'object') {
+                const totalData = Object.entries(response).map(([category, value]) => ({ category, value }));
+                const labels = totalData.map(item => item.category);
+                const values = totalData.map(item => item.value);
+                const chartData = {
+                    labels,
+                    datasets: [{
+                        label: 'Total Value',
+                        data: values,
+                        backgroundColor: chartColors,
+                        borderColor: borderColors,
+                        borderWidth: 1,
+                    }],
+                };
+                setChartData(chartData);
+                setTotal(values.reduce((acc, currentValue) => acc + currentValue, 0));
+            } else {
+                console.error('Invalid response:', response);
+            }
+        } catch (error) {
+            console.error('Error fetching total data:', error);
+        }
+    };
 
 
     return (
@@ -199,16 +152,17 @@ export default function PieChartComponent({navigation}){
                     <Text style={styles.title}>Inventory Value by Category</Text>
                     <View style={styles.picker}>
                         <Text style={styles.label}>Select Category:</Text>
-                        <Picker
-                            selectedValue={selectedCategory}
-                            onValueChange={(itemValue) => handleInputChange('category', itemValue)}
-                        >
-                            {categories.map((category, index) => (
-                                <Picker.Item key={index} label={category} value={category} />
-                            ))}
-                            <Picker.Item label="Total Values" value="totalValues" />
-                        </Picker>
-                </View>
+                        <FlatList
+                            data={categories}
+                            keyExtractor={(item, index) => index.toString()}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity onPress={() => handleInputChange('category', item)}>
+                                    <Text>{item}</Text>
+                                </TouchableOpacity>
+                            )}
+                        />
+                    </View>
+
                     <View style={styles.info}>
                         <Text style={styles.label}>Total expense: ${total.toFixed(2)}</Text>
                     </View>
@@ -231,7 +185,6 @@ const styles = StyleSheet.create({
     picker:{
         width: '50%',
         alignSelf: 'center',
-        height: 60,
         backgroundColor: '#717336',
         borderColor: '#5d5e24',
         borderWidth: 2,
